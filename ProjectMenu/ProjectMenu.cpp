58b3de0a952b9e39d7633ce4761ba938e375d06a
hophop228 <126551413+hophop228@users.noreply.github.com>
2023-12-13 16:34:54 +0300

    akrivoshein Start porject Menu

---
+++ ProjectMenu/ProjectMenu.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main() {
+	std::setlocale(LC_ALL, "");
+
+	int input;
+
+	do {
+		std::cout << "Привет, друг!" << std::endl;
+		std::cout << "1 - хочу учиться математике" << std::endl;
+		std::cout << "0 - хочу лежать" << std::endl;
+		std::cout << "Ввод: ";
+
+		std::cin >> input;
+		if (input == 1) {
+			//TODO
+		}
+		else if (input == 0) {
+			exit(0);
+		}
+
+		std::cout << std::endl;
+
+	} while (true);
+
+	return 0;
+}
